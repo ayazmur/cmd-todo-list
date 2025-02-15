72ff93d53934b1522f4b1b274e86d3883d442bd5
ayaz <ayazikmur@gmail.com>
2025-02-15 17:15:58 +0500

    refactor: использовал форматтер кода black

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import os
 import argparse
 import json
 from uuid import uuid4
+
+
 class Task:
     def __init__(self, name):
         self.name = name
         self.active = True
+
 
 class ConsoleToDo:
     def __init__(self):
@@ -21,9 +24,9 @@ class ConsoleToDo:
         :return: данные из json
         """
         if not os.path.exists(json_name) or os.path.getsize(json_name) == 0:
-            with open(json_name, 'w', encoding="utf-8") as file:
+            with open(json_name, "w", encoding="utf-8") as file:
                 json.dump({}, file, ensure_ascii=False)
-        with open(json_name, 'r', encoding="utf-8") as file:
+        with open(json_name, "r", encoding="utf-8") as file:
             data = json.load(file)
             return data
 
@@ -34,7 +37,7 @@ class ConsoleToDo:
         :param json_name: полное имя файла json в корне
         :param data: данные к сохранению в json
         """
-        with open(json_name, 'w') as file:
+        with open(json_name, "w") as file:
             json.dump(data, file)
 
     def print_tasks(self):
@@ -56,6 +59,7 @@ class ConsoleToDo:
         """
         idf = str(uuid4())
         return idf
+
     def add_task(self):
         """
         Добавление таска
@@ -67,7 +71,7 @@ class ConsoleToDo:
             print("Пустой ввод")
             return
         task = Task(task)
-        self.data[idf] = {'name': task.name, 'active': task.active}
+        self.data[idf] = {"name": task.name, "active": task.active}
         self.save_json(self.json_name, self.data)
 
     def edit_task(self):
@@ -79,22 +83,22 @@ class ConsoleToDo:
             print(f"Редактируемый таск: '{idf} {self.data[idf]}'")
             new_task = input(f"Введите новый таск:")
             if new_task not in self.data.values():
-                self.data[idf]['name'] = new_task
+                self.data[idf]["name"] = new_task
                 self.save_json(self.json_name, self.data)
             else:
                 print(f"Таск '{new_task}' уже существует.")
         else:
             print(f"Таск '{idf}' не существует.")
 
-
     def mark_task(self):
         """
         Завершение таска
         """
         idf = input("Введите id таска для завершения: ")
-        self.data[idf]['active'] = False
+        self.data[idf]["active"] = False
         print(f"Таск '{self.data[idf]}' завершен.")
         self.save_json(self.json_name, self.data)
+
     def delete_task(self):
         """
         Удаление таска
@@ -113,15 +117,22 @@ class ConsoleToDo:
         """
         os.remove(self.json_name)
 
+
 console_to_do = ConsoleToDo()
 parser = argparse.ArgumentParser()
+
+
 def __add_arguments():
-    parser.add_argument('-pt', '--print_tasks', action='store_true', help="Print tasks to cli")
-    parser.add_argument('-at', '--add_task', action='store_true', help="Add task")
-    parser.add_argument('-et', '--edit_tasks', action='store_true', help="Edit task")
-    parser.add_argument('-ddb', '--delete_data_base', action='store_true', help="Delete DB")
-    parser.add_argument('-mt', '--mark_task', action='store_true', help="Mark task")
-    parser.add_argument('-dt', '--delete_task', action='store_true', help="Delete task")
+    parser.add_argument(
+        "-pt", "--print_tasks", action="store_true", help="Print tasks to cli"
+    )
+    parser.add_argument("-at", "--add_task", action="store_true", help="Add task")
+    parser.add_argument("-et", "--edit_tasks", action="store_true", help="Edit task")
+    parser.add_argument(
+        "-ddb", "--delete_data_base", action="store_true", help="Delete DB"
+    )
+    parser.add_argument("-mt", "--mark_task", action="store_true", help="Mark task")
+    parser.add_argument("-dt", "--delete_task", action="store_true", help="Delete task")
     args = parser.parse_args()
     if args.print_tasks:
         console_to_do.print_tasks()
@@ -135,6 +146,6 @@ def __add_arguments():
         console_to_do.mark_task()
     elif args.delete_task:
         console_to_do.delete_task()
+
+
 __add_arguments()
-
-
