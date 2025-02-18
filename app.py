2c79212114b72f9f8114e35936af098784808a20
ayaz <ayazikmur@gmail.com>
2025-02-18 23:00:07 +0500

    feat: наработки по enum

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from console import ConsoleToDo
 from argparser import MyParser
-
+from start_type import StartType
+start_type = StartType.ARGPARSE_START
 if __name__ == "__main__":
     console_to_do = ConsoleToDo()
     my_parser = MyParser(console_to_do)
     my_parser.add_arguments()
-    console_to_do.start_console()
+    if start_type == StartType.WHILE_START:
+        console_to_do.start_console()
+
+
---
+++ start_type.py
@@ -0,0 +1,4 @@
+from enum import Enum
+class StartType(Enum):
+    WHILE_START= 0
+    ARGPARSE_START= 1
