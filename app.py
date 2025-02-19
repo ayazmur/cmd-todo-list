6aface6869b214a25aee7ea18fc0109e0d66bf8e
ayaz <ayazikmur@gmail.com>
2025-02-19 09:23:33 +0500

    feat: вариант enum

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from start_type import StartType
 start_type = StartType.ARGPARSE_START
 if __name__ == "__main__":
     console_to_do = ConsoleToDo()
-    my_parser = MyParser(console_to_do)
-    my_parser.add_arguments()
     if start_type == StartType.WHILE_START:
         console_to_do.start_console()
+    elif start_type == StartType.ARGPARSE_START:
+        my_parser = MyParser(console_to_do)
+        my_parser.add_arguments()
+    else:
+        raise ValueError("Неизвестный тип запуска программы")
 
 
