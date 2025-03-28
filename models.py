2de62347295b9d73306d0efb3e65d2c68d962e14
ayaz <ayazikmur@gmail.com>
2025-03-28 12:07:35 +0500

    feat: добавил комментарии к колонкам БД

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ from database import Base
 
 class Task(Base):
     __tablename__ = "tasks"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    task_text = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, comment="Уникальный идентификатор задачи")
+    task_text = Column(String, nullable=False, comment="Текст задачи")
+    is_active = Column(Boolean, default=True, comment="Флаг активности задачи. True - задача активна. False - задача завершена")
